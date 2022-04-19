82f23fd565e6fba7e2ff81067b0317a07c20f505
Austin Stockslager <austinstockslager@gmail.com>
2022-04-19 10:53:34 -0400

    Initial Commit

---
+++ Tic Tac Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic Tac Toe
+//
+//  Created by Austin Alan Stockslager (Student) on 4/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
